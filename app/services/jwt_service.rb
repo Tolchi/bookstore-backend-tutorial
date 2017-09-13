74396d6274cfd940de82db9dc859b14a49b9c6cd
Se Hoon Park <sehoon.park@gmail.com>
2017-09-13 18:30:04 -0300

    Added new README.md with codelicmate and trivis-ci status. Commented out the problmatic part of spec. Also fixed jwt-service file.

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -4,9 +4,9 @@ class JwtService
   end
 
   def self.decode(token)
-    body = HashWithIndifferentAccess.new
     body, = JWT.decode(token, Rails.application.secrets.secret_key_base,
                        true, algorithm: 'HS256')
+    HashWithIndifferentAccess.new(body)
   rescue JWT::ExpiredSignature
     nil
   end
